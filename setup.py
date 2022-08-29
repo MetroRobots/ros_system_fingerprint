89af9a776fe6f811d8c9527eb2cf2dcd6b61cbee
David V. Lu <davidvlu@gmail.com>
2022-08-29 13:08:37 -0400

    ROS 2 Conversion

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
-package_info = generate_distutils_setup(
+setup(
     packages=['ros_system_fingerprint'],
     package_dir={'': 'src'},
+    data_files=[
+        ('share/ros_system_fingerprint', ['package.xml']),
+        ('share/ament_index/resource_index/packages', ['resource/ros_system_fingerprint']),
+    ],
+    version='0.3.0',
+    description='The ros_system_fingerprint package',
+    license='BSD 2-clause',
+    maintainer='David V. Lu!!',
+    maintainer_email='davidvlu@gmail.com',
+    entry_points={'console_scripts': ['imprint = ros_system_fingerprint.imprint:main']},
 )
-
-setup(**package_info)
